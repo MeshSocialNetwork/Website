14268429274b73c9fa048ebbd3a90ccd0198eaeb
jonasrdl <jonasriedel@protonmail.com>
2022-02-21 08:19:18 +0100

    Add Settings component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path={'/'} element={<App />} />
             <Route path={'/profile'} element={<Profile />} />
+            <Route path={'/settings'} element={<Settings />} />
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
