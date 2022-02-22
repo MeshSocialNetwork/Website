77910003c0667cddea5b13cabc32b57886845bd3
jonasrdl <jonasriedel@protonmail.com>
2022-02-22 09:20:52 +0100

    Add team page, cors needs to be fixed

--- src/components/pages/Team/Team.jsx
+++ src/components/pages/Team/Team.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+import Navbar from '../../Navbar/Navbar';
+import TeamMember from './TeamMember/TeamMember';
 
 class Team extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            teamMembers: []
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://meshnetwork.app/api/discord')
+            .then((result) => {
+                let teamMembers = [];
+
+                result.data.forEach((user) => {
+                    let gif = user.avatar.includes('a_');
+
+                    teamMembers.push(
+                        <TeamMember role={'Admin'} user={user} gif={gif} />
+                    );
+                });
+
+                this.setState({
+                    teamMembers: teamMembers
+                });
+            })
+            .catch((error) => console.log(error));
+    }
+
     render() {
-        return <p>Team</p>;
+        return (
+            <>
+                <Navbar />
+
+                {this.state.teamMembers}
+            </>
+        );
     }
 }
 
---
+++ src/components/pages/Team/TeamMember/TeamMember.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class TeamMember extends React.Component {
+    getDivClassName() {
+        switch (this.props.user.status) {
+            case 'offline':
+                return 'card-user offline-status';
+            case 'idle':
+                return 'card-user idle-status';
+            case 'dnd':
+                return 'card-user dnd-status';
+            case 'online':
+                return 'card-user online-status';
+            default:
+                return 'card-user offline-status';
+        }
+    }
+
+    render(props) {
+        return (
+            <>
+                <div className='card'>
+                    <div className={this.getDivClassName()}>
+                        <div className='user-text'>
+                            <h1 className='card-username'>
+                                {this.props.user.username}
+                            </h1>
+                            <h2 className='card-role'>{this.props.role}</h2>
+                        </div>
+                        <div className='avatar'>
+                            {this.props.gif ? (
+                                <img
+                                    alt='avatar'
+                                    src={`https://cdn.discordapp.com/avatars/${this.props.user.id}/${this.props.user.avatar}.gif?size=64`}
+                                />
+                            ) : (
+                                <img
+                                    alt='avatar'
+                                    src={`https://cdn.discordapp.com/avatars/${this.props.user.id}/${this.props.user.avatar}.webp?size=64`}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default TeamMember;
