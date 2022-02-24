450d3fa2d650afeab0390f936c56ee6138d76bcb
jonasrdl <jonasriedel@protonmail.com>
2022-02-24 15:32:20 +0100

    Implement profile page

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import App from '../../../App';
+import axios from 'axios';
+const config = require('../../../config.json');
 
 const Profile = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [verified, setVerified] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get(config.CHECK_LOGIN_ENDPOINT)
+            .then((result) => {
+                const username = setUsername(result.data.name);
+                const email = setEmail(result.data.email);
+                const verified = setVerified(result.data.emailVerified);
+            })
+            .catch((error) => {
+                console.log(error.response.data.message);
+
+                window.location = '/login';
+            });
+    }, []);
+
     return (
         <>
             <App />
-            <p>Hello World</p>
+
+            <h1>{username}</h1>
+            <h2>{email}</h2>
+            <h3>{verified}</h3>
         </>
     );
 };
