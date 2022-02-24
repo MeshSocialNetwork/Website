fb15491bba91c76a0e7ea4978a5f3780d4a54a8d
jonasrdl <jonasriedel@protonmail.com>
2022-02-24 16:22:17 +0100

    WIP: Profile picture

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -1,12 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import App from '../../../App';
 import axios from 'axios';
+
 const config = require('../../../config.json');
 
 const Profile = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [verified, setVerified] = useState(false);
+    const [image, setImage] = useState(null);
+    const [cdnID, setCdnID] = useState('');
+
+    const sendImageToCDN = () => {
+        axios
+            .get(config.CHOOSE_CDN_ENDPOINT)
+            .then((result) => {
+                setCdnID(result.data.cdn.id);
+            })
+            .catch((error) => console.log(error));
+
+        const formData = new FormData();
+        formData.append('file', image);
+
+        console.log(cdnID);
+
+        axios
+            .post(config.CHOOSE_CDN_ENDPOINT + `${cdnID}/image`, formData)
+            .then((result) => {
+                console.log('image upload req made');
+
+                console.log(result);
+            })
+            .catch((error) => console.log(error));
+    };
 
     useEffect(() => {
         axios
@@ -27,9 +53,21 @@ const Profile = () => {
         <>
             <App />
 
+            <img src={''} alt={''} className={'avatar'} />
+
             <h1>{username}</h1>
             <h2>{email}</h2>
             <h3>{verified}</h3>
+
+            <input
+                type={'file'}
+                name={'upload-image'}
+                onChange={(event) => {
+                    console.log(event.target.files[0]);
+                    setImage(event.target.files[0]);
+                    sendImageToCDN();
+                }}
+            />
         </>
     );
 };
