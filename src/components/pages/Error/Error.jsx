9a945091b798f0ff8d6cbc6cd2da7da1931c2853
jonasrdl <jonasriedel@protonmail.com>
2022-02-27 20:58:55 +0100

    Fix react error

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -4,29 +4,31 @@ import { toast } from 'react-toastify';
 const Error = () => {
     const { state } = useLocation();
 
-    switch (state.error) {
-        case 'no-user':
-            toast.error('This user does not exist!', {
-                position: 'top-right',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                progress: undefined
-            });
-            break;
-        default:
-            toast.error('Something went wrong!', {
-                position: 'top-right',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                progress: undefined
-            });
-            break;
+    if (state) {
+        switch (state.error) {
+            case 'no-user':
+                toast.error('This user does not exist!', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    progress: undefined
+                });
+                break;
+            default:
+                toast.error('Something went wrong!', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    progress: undefined
+                });
+                break;
+        }
     }
 
     return <Link to={'/'}>Back home!</Link>;
